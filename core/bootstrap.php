1bfcbc7fd1456789c8a9b6353b218e323939cf01
tharindu830 <80748866+tharindu830@users.noreply.github.com>
2021-03-16 14:40:51 +0530

    Test1
    Just for testing

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -2,6 +2,7 @@
  // load configuration and helper functions
  require_once(ROOT . DS . 'config' . DS . 'config.php');
  require_once(ROOT . DS . 'app' . DS . 'lib' . DS . 'helpers' . DS . 'functions.php');
+ // require_once(ROOT . DS . 'app' . DS . 'lib' . DS . 'helpers' . DS . 'functions.php');
 
 // Autoload classes
 function __autoload($className) {
