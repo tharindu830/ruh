9cbe8941357b0df377d2da5f6f4798a6d8b5a52c
tharindu830 <80748866+tharindu830@users.noreply.github.com>
2021-03-16 14:33:38 +0530

    Initializing repo

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+define('DEBUG' , true);
+ define('DEFAULT_CONTROLLER', 'HOME'); // default controller is there isn't one defined in the url
+ define('DEFAULT_LAYOUT', 'default'); //if no layout is set in the controller use this layout
+
+ define('PROOT', '/ruh/'); // set this ti '/' for a alive server
+
+ define('SITE_TITLE', 'R MVC FRAMEWORK'); // This will be used if no site titile is set
